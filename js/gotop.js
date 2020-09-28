41e7dd2ded9e3c67cb5722b09b32708dcbd2f79b
Virginia Bernaschina <vbernaschina@hotmail.com>
2020-09-28 09:56:25 -0300

    archivos base pre git

---
+++ js/gotop.js
@@ -0,0 +1,17 @@
+/* boton_to_top */
+window.onscroll = function(){
+  if(document.documentElement.scrollTop > 100){
+  document.querySelector('.go-top-container')
+  .classList.add('show');
+  }else{
+    document.querySelector('.go-top-container')
+    .classList.remove('show');
+  }
+}
+document.querySelector('.go-top-container')
+.addEventListener('click', () =>{
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+  });
+});
